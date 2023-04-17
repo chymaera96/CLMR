b2a640033f32e57313aa34f6850cf54fcd07a852
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-04-17 20:15:44 +0100

    bug fix

--- generation_pipeline.sh
+++ generation_pipeline.sh
@@ -1,4 +1,4 @@
 gdown https://drive.google.com/uc?id=1eShRtrCNUuxNtRzsQpR1ERpYrMFa22dj
 unzip -q archive.zip
 rm Data/genres_original/jazz/jazz.00054.wav
-python -m extract_embeddings.py
+python extract_embeddings.py
