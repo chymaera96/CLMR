b29893130029c9126b6c1f4a4158f65ebc04f557
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-04-21 15:49:35 +0100

    refactoring generation pipeline for Colab

--- generation_pipeline.sh
+++ generation_pipeline.sh
@@ -1,4 +1,5 @@
+wget -nc https://github.com/Spijkervet/CLMR/releases/download/2.1/clmr_magnatagatune_mlp.zip
+unzip -o clmr_magnatagatune_mlp.zip
 gdown https://drive.google.com/uc?id=1eShRtrCNUuxNtRzsQpR1ERpYrMFa22dj
 unzip -q archive.zip
 rm Data/genres_original/jazz/jazz.00054.wav
-python extract_embeddings.py
